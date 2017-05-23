b67030a877898a061d1b189b62c9835c8564e371
g31pranjal <g31.pranjal@gmail.com>
2017-05-23 14:24:57 -0500

    changes.

--- wikie/search/urls.py
+++ wikie/search/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^results/$', views.fetchResults, name='fetchResults'),
+	url(r'^results$', views.fetchResults, name='fetchResults'),
 	url(r'^goto/$', views.goto, name='goto'),
 
 ]
